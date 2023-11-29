88625e78cfba66be9c0e5345af07e363e1d3d0bf
boylek42 <boylekevin2000@gmail.com>
2023-11-29 19:18:02 +0000

    Created the function "printSV" which prints a string_view.
    I am aware that this is unneccesary as a string_view parametre can implicitly convert a string argument to a string_view type.

--- strings.cpp
+++ strings.cpp
@@ -2,14 +2,23 @@
 #include <string>
 #include <string_view>
 
-void printString(std::string str) 
+void printS(std::string str) 
+{
+	std::cout << str << '\n';
+}
+
+void printSV(std::string_view str)
 {
 	std::cout << str << '\n';
 }
 
 int main() 
 {
-	printString("This is a string!");
+	using namespace std::string_view_literals;
+	using namespace std::string_literals;
+
+	printS("This is a string!"s);
+	printSV("This is a string view"sv);
 
 	return 0;
 }
